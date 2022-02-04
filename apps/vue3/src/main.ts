04a2a44dd519ed3410f9e302d5dfd0b25b85f4a8
KE.YANG <ke.yang@outlook.com>
2022-02-04 12:21:37 +0800

    vite

--- apps/vue3/src/main.ts
+++ apps/vue3/src/main.ts
@@ -109,5 +109,10 @@ app
   .component(Authority.name, Authority);
 
 useIcons(app);
+const container = document.querySelector("#app")
+if(container){
+  app.mount('#app');
+}else{
+  app.mount('#vite-app');
+}
 
-app.mount('#app');
--- playground/micro-app/src/views/app3.tsx
+++ playground/micro-app/src/views/app3.tsx
@@ -6,6 +6,6 @@ import config from '@/config/apps.config'
 export default defineComponent({
     setup() {
         const url = `${config.app3}/app3`
-        return () => <micro-app name="app3" url={url} baseroute="/app3" shadowdom={true} />
+        return () => <micro-app name="app3" url={url} baseroute="/app3"/>
     }
 })
