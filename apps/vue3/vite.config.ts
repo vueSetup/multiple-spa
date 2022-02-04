dd85d913c28db7e552622b5c7c7ad941129a33b9
buzzlightyear <794758159@qq.com>
2022-02-04 16:45:46 +0800

    app1

--- apps/vue3/vite.config.ts
+++ apps/vue3/vite.config.ts
@@ -4,8 +4,8 @@ import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 import { getThemeVariables } from 'ant-design-vue/dist/theme';
 import { additionalData } from './build/themeConfig';
-import createMockServer from './build/mockServer';
-import path from 'path';
+import { join } from 'path';
+import { writeFileSync } from 'fs';
 
 export default ({ mode }: ConfigEnv): UserConfig => {
   const root = process.cwd();
@@ -18,33 +18,33 @@ export default ({ mode }: ConfigEnv): UserConfig => {
       'process.env.VUE_APP_API_BASE_URL': JSON.stringify(env.VITE_APP_API_BASE_URL),
     },
     plugins: [
-      vue(), 
+      vue(),
       vueJsx(),
       // 自定义插件
-    (function () {
-      let basePath = ''
-      return {
-        name: "vite:micro-app",
-        apply: 'build',
-        configResolved(config) {
-          basePath = `${config.base}${config.build.assetsDir}/`
-        },
-        writeBundle (options, bundle) {
-          for (const chunkName in bundle) {
-            if (Object.prototype.hasOwnProperty.call(bundle, chunkName)) {
-              const chunk = bundle[chunkName]
-              if (chunk.fileName && chunk.fileName.endsWith('.js')) {
-                chunk.code = chunk.code.replace(/(from|import\()(\s*['"])(\.\.?\/)/g, (all, $1, $2, $3) => {
-                  return all.replace($3, new URL($3, basePath))
-                })
-                const fullPath = join(options.dir, chunk.fileName)
-                writeFileSync(fullPath, chunk.code)
+      (function () {
+        let basePath = ''
+        return {
+          name: 'vite:micro-app',
+          apply: 'build',
+          configResolved(config) {
+            basePath = `${config.base}${config.build.assetsDir}/`
+          },
+          writeBundle(options, bundle) {
+            for (const chunkName in bundle) {
+              if (Object.prototype.hasOwnProperty.call(bundle, chunkName)) {
+                const chunk = bundle[chunkName]
+                if (chunk.fileName && chunk.fileName.endsWith('.js')) {
+                  chunk.code = chunk.code.replace(/(from|import\()(\s*['"])(\.\.?\/)/g, (all, $1, $2, $3) => {
+                    return all.replace($3, new URL($3, basePath))
+                  })
+                  const fullPath = join(options.dir, chunk.fileName)
+                  writeFileSync(fullPath, chunk.code)
+                }
               }
             }
-          }
-        },
-      }
-    })() as any],
+          },
+        }
+      })() as any],
     build: {
       cssCodeSplit: false,
       chunkSizeWarningLimit: 2048,
@@ -62,8 +62,8 @@ export default ({ mode }: ConfigEnv): UserConfig => {
       alias: {
         moment$: 'moment/dist/moment.js',
         'moment/locale': 'moment/dist/locale',
-        '~@': path.join(__dirname, './src'),
-        '@': path.join(__dirname, './src'),
+        '~@': join(__dirname, './src'),
+        '@': join(__dirname, './src'),
         vue: 'vue/dist/vue.esm-bundler.js',
       },
     },
