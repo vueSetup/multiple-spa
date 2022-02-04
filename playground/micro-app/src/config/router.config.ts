813f8b3273b387c1e40afc97e8664f9b10fc0b58
KE.YANG <ke.yang@outlook.com>
2022-02-04 12:26:08 +0800

    weclome

--- playground/micro-app/src/config/router.config.ts
+++ playground/micro-app/src/config/router.config.ts
@@ -1,21 +1,28 @@
 import { RouteRecordRaw, RouterView } from 'vue-router'
 
-export const constantRouterMap: RouteRecordRaw[] = [{
-    path: '/app1/:page*',
-    name: 'app1',
-    component: () =>
-        import(/* webpackChunkName: "app1" */ '@/views/app1'),
-    meta: { title: '加载中' }
-}, {
-    path: '/app2/:page*',
-    name: 'app2',
-    component: () =>
-        import(/* webpackChunkName: "app2" */ '@/views/app2'),
-    meta: { title: '加载中' }
-}, {
-    path: '/app3/:page*',
-    name: 'app3',
-    component: () =>
-        import(/* webpackChunkName: "app3" */ '@/views/app3'),
-    meta: { title: '加载中' }
-}]
+export const constantRouterMap: RouteRecordRaw[] = [
+    {
+        path: '/',
+        name: 'weclome',
+        component: () =>
+            import(/* webpackChunkName: "app1" */ '@/views/weclome.vue'),
+        meta: { title: '加载中' }
+    }, {
+        path: '/app1/:page*',
+        name: 'app1',
+        component: () =>
+            import(/* webpackChunkName: "app1" */ '@/views/app1'),
+        meta: { title: '加载中' }
+    }, {
+        path: '/app2/:page*',
+        name: 'app2',
+        component: () =>
+            import(/* webpackChunkName: "app2" */ '@/views/app2'),
+        meta: { title: '加载中' }
+    }, {
+        path: '/app3/:page*',
+        name: 'app3',
+        component: () =>
+            import(/* webpackChunkName: "app3" */ '@/views/app3'),
+        meta: { title: '加载中' }
+    }]
