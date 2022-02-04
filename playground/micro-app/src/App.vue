a46020194d749e7f425db622e37cfc4d4f293e03
buzzlightyear <794758159@qq.com>
2022-02-04 12:37:08 +0800

    links

--- playground/micro-app/src/App.vue
+++ playground/micro-app/src/App.vue
@@ -1,3 +1,18 @@
 <template>
-    <router-view />
+    <div>
+        <header>
+            <router-link to="/">main app</router-link>
+            <router-link to="/app1">app1</router-link>
+            <router-link to="/app2">app2</router-link>
+            <router-link to="/app3">app3</router-link>
+        </header>
+        <router-view />
+    </div>
 </template>
+
+<style scoped>
+header a {
+    margin-right: 8px;
+    font-size: 16px;
+}
+</style>
