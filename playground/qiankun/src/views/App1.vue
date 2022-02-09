fdaa35c2bf375d9a16a7012d58cd8952cf7cdfbf
buzzlightyear <794758159@qq.com>
2022-02-09 13:10:20 +0800

    qiankun app1

--- playground/qiankun/src/views/App1.vue
+++ playground/qiankun/src/views/App1.vue
@@ -1,3 +1,8 @@
 <template>
-    <p>qiankun官方不支持vite，需要vite-plugin-qiankun插件</p>
+    <div>
+        <p>qiankun官方不支持vite，需要第三方插件</p>
+        <a href="https://github.com/tengmaoqing/vite-plugin-qiankun" target="_blank">
+            https://github.com/tengmaoqing/vite-plugin-qiankun
+        </a>
+    </div>
 </template>
